30e086b0374fb4ea4c7aaf191894e30848ddc833
k12re <83732570+k12re@users.noreply.github.com>
2022-08-09 21:57:57 +0200

    opened files

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
+//Question 1
+
+let cat = {};
