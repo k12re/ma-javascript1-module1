c82c4330277ca29abfb7c0e659a1a5527cc3cc11
k12re <83732570+k12re@users.noreply.github.com>
2022-08-12 10:39:24 +0200

    did q1-q7, started q8

--- js/script.js
+++ js/script.js
@@ -14,4 +14,53 @@ const cats = [
 
 //Question 1
 
-let cat = {};
+const cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
+
+cat.complain();
+
+//Question 2
+
+let heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+//Question 3
+
+heading.style.fontSize = "2em";
+
+//Question 4
+
+heading.classList.add("subheading");
+
+//Question 5
+
+let paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "red";
+}
+
+//Question 6
+
+let resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+//Question 7
+
+function aFunction(list) {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i].name);
+  }
+}
+
+aFunction(cats);
+
+//Question 8
+
+function createCats(cats) {}
